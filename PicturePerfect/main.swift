34aa38fe6e57eb2fe59ece0c0e0331dc19fab7a6
Jennifer Yang <yyang@lcs-mba3025.lcs.on.ca>
2018-05-23 10:47:30 -0400

    finished input section

--- PicturePerfect/main.swift
+++ PicturePerfect/main.swift
@@ -8,6 +8,27 @@ import Foundation
 // INPUT
 // Global variable to use later in program
 var countOfPhotoArrangementsToBeConsidered = 3
+var n = 0
+while 1 == 1 {
+    print("How many photo arrangements will be considered?")
+    
+    // Test 1 make sure it's not nil
+    let input = readLine()
+    guard let givenInput = input else {
+        continue
+    }
+    // Test 2 make sure it's integer
+    guard let integerInput = Int(givenInput) else {
+        continue
+    }
+    
+    // Test 3 is it in correct range
+    if integerInput < 1 || integerInput > 10 {
+        continue
+    }
+    countOfPhotoArrangementsToBeConsidered = integerInput
+    break
+}
 
 // Write a loop to actually collect the number of photo arrangements to be considered
 // e.g.: write the rest of the INPUT section
